61925c88cb863a46494e5e0f3ad6be3ceac63856
Alejandro Tapia <tapia6176@gmail.com>
2024-06-26 20:10:50 -0700

    partial login changes

--- edtech-social-app/prisma/migrations/20240625000724_init_user_table/migration.sql
+++ edtech-social-app/prisma/migrations/20240625000724_init_user_table/migration.sql
@@ -19,5 +19,21 @@ CREATE TABLE "Comment" (
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
 -- AddForeignKey
 ALTER TABLE "Comment" ADD CONSTRAINT "Comment_video_id_fkey" FOREIGN KEY ("video_id") REFERENCES "Video"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
