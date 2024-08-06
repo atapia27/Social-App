1fdb306917ef9c5a12ca24588995f890d61b2da1
Alejandro Tapia <tapia6176@gmail.com>
2024-08-05 22:06:36 -0700

    router changes

--- backend/videos/video_routers.py
+++ backend/videos/video_routers.py
@@ -1,15 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-from backend.dependencies import get_db
+from fastapi import APIRouter, HTTPException
 from backend import schemas
 import httpx
 
 BASE_URL = "https://take-home-assessment-423502.uc.r.appspot.com/api/videos"
 
 router = APIRouter()
-
-##############################################################################################
-#  helper functions for async http requests
 
 async def async_http_get(url: str, params: dict = None):
     async with httpx.AsyncClient() as client:
@@ -20,7 +15,6 @@ async def async_http_get(url: str, params: dict = None):
         except httpx.HTTPStatusError as e:
             raise HTTPException(status_code=e.response.status_code, detail=str(e))
 
-
 async def async_http_post(url: str, data: dict):
     async with httpx.AsyncClient() as client:
         try:
@@ -30,54 +24,40 @@ async def async_http_post(url: str, data: dict):
         except httpx.HTTPStatusError as e:
             raise HTTPException(status_code=e.response.status_code, detail=str(e))
 
-
-##############################################################################################
-# functions to interact with the external API
-
 async def post_video_data(video: schemas.CreateVideo):
     url = BASE_URL
-    data = video.dict()  # Assuming this method returns a dict suitable for JSON serialization
+    data = video.dict()
     return await async_http_post(url, data)
-
 
 async def get_videos_data(user_id: str):
     url = BASE_URL
     params = {"user_id": user_id}
     return await async_http_get(url, params)
 
-
 async def post_video_comments_data(comment: schemas.CreateComment):
     url = BASE_URL + "/comments"
-    data = comment.dict()  # Assuming this method returns a dict suitable for JSON serialization
+    data = comment.dict()
     return await async_http_post(url, data)
 
-
 async def get_comments_data(video_id: str):
-    url = BASE_URL + f"/comments"
+    url = BASE_URL + "/comments"
     params = {"video_id": video_id}
     return await async_http_get(url, params)
-
-
-##############################################################################################
-# API routes
 
 @router.post("/videos")
 async def post_videos(video: schemas.CreateVideo):
     data = await post_video_data(video)
     return data
 
-
 @router.get("/videos/{user_id}")
 async def get_videos(user_id: str):
     data = await get_videos_data(user_id)
     return data
 
-
 @router.post("/videos/comments")
 async def post_video_comments(comment: schemas.CreateComment):
     data = await post_video_comments_data(comment)
     return data
-
 
 @router.get("/videos/comments/{video_id}")
 async def get_comments(video_id: str):
